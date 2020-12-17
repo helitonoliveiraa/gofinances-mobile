790543dda41ed15af62a81dbe40f709422fb3797
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-12-17 20:29:14 -0300

    :bug: file with declaration of type

---
+++ src/styles/theme/styled.d.ts
@@ -0,0 +1,12 @@
+import {color} from 'react-native-reanimated';
+import 'styled-components';
+
+import {colors} from './colors';
+import {fonts} from './fonts';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: typeof colors;
+    fonts: typeof fonts;
+  }
+}
