6a605345ca9c715cac8fd3016ce13d3e5785b961
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-12-17 19:54:01 -0300

    :wrench: config header and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import {View, StatusBar, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from 'styled-components';
 
@@ -12,10 +13,20 @@ const App: React.FC = () => {
   return (
     <NavigationContainer>
       <ThemeProvider theme={styleTheme}>
-        <Routes />
+        <StatusBar barStyle="light-content" backgroundColor="#5636D3" />
+        <View style={styles.background}>
+          <Routes />
+        </View>
       </ThemeProvider>
     </NavigationContainer>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#F0F2F5',
+  },
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import {Image, Text, StyleSheet} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
+
+import logo from '../assets/logo.png';
 
 import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
@@ -8,7 +11,15 @@ const Stack = createStackNavigator();
 
 const Routes: React.FC = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerTitle: () => <Image source={logo} />,
+        headerStyle: {
+          backgroundColor: '#5636D3',
+        },
+        headerRight: () => <Text style={styles.data}>18 de dez</Text>,
+      }}>
       <Stack.Screen name="Dashboard" component={Dashboard} />
       <Stack.Screen name="Import" component={Import} />
     </Stack.Navigator>
@@ -16,3 +27,10 @@ const Routes: React.FC = () => {
 };
 
 export default Routes;
+
+const styles = StyleSheet.create({
+  data: {
+    color: '#969CB2',
+    marginRight: 20,
+  },
+});
