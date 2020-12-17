d871e9aaad876e1bb03eadb99ac6e65905e32ddd
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-12-17 16:53:43 -0300

    :lipstick: chore: configure global colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {ThemeProvider} from 'styled-components';
+
+import {styleTheme} from './styles/theme';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>GoFinances</Text>
-    </View>
+    <ThemeProvider theme={styleTheme}>
+      <View>
+        <Text>GoFinances</Text>
+      </View>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,11 @@
+import {DefaultTheme} from 'styled-components';
+
+import {colors} from './colors';
+import {fonts} from './fonts';
+
+const styleTheme: DefaultTheme = {
+  colors: colors,
+  fonts: fonts,
+};
+
+export {styleTheme};
