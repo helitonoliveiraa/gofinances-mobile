29c1ce709a29a67e8ceffb4c11bbe70b7d135b32
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-12-17 16:26:30 -0300

    :tada: Initial commit

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  blue: '#5636D3',
+  orange: '#FF872C',
+  green: '#12A454',
+  red: '#E83F5B',
+  shapeMain: '#FFFFFF',
+  title: '#363F5F',
+  text: '#969CB2',
+  background: '#F0F2F5',
+};
