f02926988b83c2cbcbb9d87431273c035c0ac757
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-12-18 14:44:33 -0300

    :recycle: chore: change structure

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {
+  Container,
+  Wrapper,
+  CardContainer,
+  Card,
+  CardTitle,
+  CardPrice,
+  CardDescription,
+  ArrowIcon,
+  TotalIcon,
+} from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Wrapper />
+      <CardContainer>
+        <Card>
+          <CardTitle>Entradas</CardTitle>
+          <ArrowIcon name="arrow-up" size={40} color="#12A454" />
+
+          <CardPrice>R$ 18.500,00</CardPrice>
+          <CardDescription>Última entrada dia 13 abril</CardDescription>
+        </Card>
+
+        <Card>
+          <CardTitle>Saídas</CardTitle>
+          <ArrowIcon name="arrow-down" size={40} color="#E83F5B" />
+
+          <CardPrice>R$ 18.500,00</CardPrice>
+          <CardDescription>Última entrada dia 13 abril</CardDescription>
+        </Card>
+
+        <Card total>
+          <CardTitle total>Total</CardTitle>
+          <TotalIcon name="dollar-sign" size={40} color="#FFFFFF" />
+
+          <CardPrice total>R$ 18.500,00</CardPrice>
+          <CardDescription total>Última entrada dia 13 abril</CardDescription>
+        </Card>
+      </CardContainer>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,82 @@
-import styled from 'styled-components/native';
+import styled, {css} from 'styled-components/native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
 
-export const Container = styled.View``;
+interface TotalProps {
+  total?: boolean;
+}
+
+export const Container = styled.SafeAreaView`
+  /* flex: 1; */
+`;
+
+export const Wrapper = styled.View`
+  background: ${({theme}) => theme.colors.blue};
+  height: 180px;
+`;
+
+export const CardContainer = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  paddingHorizontal: 18,
+})`
+  margin-top: -160px;
+`;
+
+export const Card = styled.View<TotalProps>`
+  ${({total, theme}) =>
+    theme &&
+    css`
+      background: ${total ? theme.colors.orange : theme.colors.shapeMain};
+      position: relative;
+      width: 300px;
+      padding: 19px 23px 42px;
+      margin-right: 16px;
+      border-radius: 5px;
+      elevation: 5;
+    `}
+`;
+
+export const CardTitle = styled.Text<TotalProps>`
+  ${({theme, total}) =>
+    theme &&
+    css`
+      color: ${total ? theme.colors.shapeMain : theme.colors.title};
+      font-family: ${theme.fonts['Poppins-Regular']};
+      font-size: 14px;
+    `}
+`;
+
+export const CardPrice = styled.Text<TotalProps>`
+  ${({total, theme}) =>
+    theme &&
+    css`
+      color: ${total ? theme.colors.shapeMain : theme.colors.title};
+      font-family: ${theme.fonts['Poppins-Regular']};
+      font-size: 30px;
+      margin-top: 55.5px;
+    `}
+`;
+
+export const CardDescription = styled.Text<TotalProps>`
+  ${({theme, total}) =>
+    theme &&
+    css`
+      color: ${total ? theme.colors.shapeMain : theme.colors.text};
+      font-family: ${theme.fonts['Poppins-Regular']};
+      font-size: 12px;
+      margin-top: 0;
+    `}
+`;
+
+export const TotalIcon = styled(FeatherIcon)`
+  position: absolute;
+  top: 20px;
+  right: 23px;
+`;
+
+export const ArrowIcon = styled(EvilIcon)`
+  position: absolute;
+  top: 20px;
+  right: 23px;
+`;
