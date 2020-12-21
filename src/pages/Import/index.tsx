e174afc957973184efbc6cff5b731bbf2e89aebe
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-12-21 14:28:17 -0300

    :sparkles: feat: register transation

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {
+  Container,
+  Title,
+  Input,
+  ButtonsContainer,
+  Button,
+  ButtonText,
+  Icon,
+  SubmitButton,
+  SubmitButtonText,
+} from './styles';
 
 const Import: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Title>Cadastro</Title>
+
+      <Input placeholder="Nome" />
+
+      <Input placeholder="Preço" style={{marginTop: 0}} />
+
+      <ButtonsContainer>
+        <Button>
+          <Icon name="arrow-up" size={30} />
+          <ButtonText>Income</ButtonText>
+        </Button>
+
+        <Button>
+          <Icon name="arrow-down" size={30} />
+          <ButtonText>Outcome</ButtonText>
+        </Button>
+      </ButtonsContainer>
+
+      <Input placeholder="Categoria" />
+
+      <SubmitButton>
+        <SubmitButtonText>Enviar</SubmitButtonText>
+      </SubmitButton>
+    </Container>
+  );
 };
 
 export default Import;
--- src/pages/Import/styles.ts
+++ src/pages/Import/styles.ts
@@ -1,3 +1,95 @@
-import styled from 'styled-components/native';
+import styled, {css} from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import {lighten} from 'polished';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  padding: 0 16px;
+`;
+
+export const Title = styled.Text`
+  ${({theme}) =>
+    theme &&
+    css`
+      color: #333;
+      font-size: 20px;
+      margin-top: 24px;
+      font-family: ${theme.fonts['Poppins-Regular']};
+    `}
+`;
+
+export const Input = styled.TextInput`
+  ${({theme}) =>
+    theme &&
+    css`
+      background: ${theme.colors.shapeMain};
+      height: 50px;
+      padding: 0 16px;
+      margin: 24px 0 16px;
+      border-radius: 5px;
+      font-family: ${theme.fonts['Poppins-Regular']};
+    `}
+`;
+
+export const ButtonsContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 16px;
+`;
+
+export const Button = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.5,
+})`
+  height: 50px;
+  width: 49%;
+  border-width: 2px;
+  border-style: solid;
+  border-radius: 5px;
+  border-color: ${({theme}) => lighten(0.26, theme.colors.text)};
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.Text`
+  ${({theme}) =>
+    theme &&
+    css`
+      font-family: ${theme.fonts['Poppins-Regular']};
+      color: ${theme.colors.title};
+      font-size: 14px;
+    `}
+`;
+
+export const Icon = styled(EvilIcon)`
+  ${({name, theme}) =>
+    theme &&
+    css`
+      color: ${name === 'arrow-up' ? theme.colors.green : theme.colors.red};
+      margin-right: 12px;
+    `}
+`;
+
+export const SubmitButton = styled(RectButton)`
+  ${({theme}) =>
+    theme &&
+    css`
+      height: 50px;
+      border-radius: 5px;
+      background: ${theme.colors.orange};
+      align-items: center;
+      justify-content: center;
+    `}
+`;
+
+export const SubmitButtonText = styled.Text`
+  ${({theme}) =>
+    theme &&
+    css`
+      font-size: 14px;
+      font-family: ${theme.fonts['Poppins-Medium']};
+      color: ${theme.colors.shapeMain};
+    `}
+`;
